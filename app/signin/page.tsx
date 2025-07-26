cccfb7d8bab83e061fce12c1d5dbbff729ddbb46
Omghadge <omghadge400@gmail.com>
2025-07-26 17:29:18 +0530

    wrapped signin signup in suspense

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,11 +1,18 @@
 "use client";
+import { Suspense } from "react";
 import { AuthPage } from "@/components/AuthPage";
 import { useSearchParams } from "next/navigation";
 
-export default function Signin(){
-    const searchParams = useSearchParams();
-    const redirect = searchParams.get('redirect') || undefined;
-    return(
-        <AuthPage isSignIn={true} redirect={redirect}/>
-    )
+function SigninInner() {
+  const searchParams = useSearchParams();
+  const redirect = searchParams.get('redirect') || undefined;
+  return <AuthPage isSignIn={true} redirect={redirect} />;
+}
+
+export default function Signin() {
+  return (
+    <Suspense>
+      <SigninInner />
+    </Suspense>
+  );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,18 @@
 "use client";
+import { Suspense } from "react";
 import { AuthPage } from "@/components/AuthPage";
 import { useSearchParams } from "next/navigation";
 
-export default function Signup(){
+function SignupInner(){
     const searchParams = useSearchParams();
     const redirect = searchParams.get('redirect') || undefined;
+    return <AuthPage isSignIn={false} redirect={redirect}/>
+}
+export default function Signup(){
+
     return(
-        <AuthPage isSignIn={false} redirect={redirect}/>
+    <Suspense>
+        <SignupInner/>
+    </Suspense>
     )
 }
